eb2179b6b358d56410fec6d1b827923ab95671e8
Shree <shreekumarf@gmail.com>
2022-10-16 23:33:26 +0530

    Day-30

--- Python-code/Day_30/main.py
+++ Python-code/Day_30/main.py
@@ -57,7 +57,20 @@ def save():
 
 #--------------------------- Find Password ------------------------------#
 def find_password():
-    wesite = website_entry.get()
+    website = website_entry.get()
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File Found..!!")
+
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email:{email}\nPassowrd:{password}")
+        else:
+            messagebox.showinfo(title="Error",message=f"No details for {website} exists.")
 
 
 #--------------------------- UI SETUP ------------------------------#
