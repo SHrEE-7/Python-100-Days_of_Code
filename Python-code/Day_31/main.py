5abceffa8bb260d299da71be7995bacda99c406c
Shree <shreekumarf@gmail.com>
2022-10-25 00:00:37 +0530

    Day-31

---
+++ Python-code/Day_31/main.py
@@ -0,0 +1,18 @@
+from tkinter import *
+from turtle import position 
+
+BACKGROUND_COLOR = "#B1DDC6"
+
+window = Tk()
+window.title("Flashy")
+
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+canvas = Canvas(width=800,height=529)
+card_front_img = PhotoImage(file="Images/front.png")
+canvas.create_image(480,263,image=card_front_img)
+canvas.config(bg=BACKGROUND_COLOR,highlightthickness=0)
+canvas.grid(row=0,column=0)
+
+
+window.mainloop() 
