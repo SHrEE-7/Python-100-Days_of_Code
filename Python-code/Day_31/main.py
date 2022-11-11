3affa27c7b9ebdeb1ad32795c7c4fe75db218a0d
Shree <shreekumarf@gmail.com>
2022-11-11 23:30:30 +0530

    Day-31

--- Python-code/Day_31/main.py
+++ Python-code/Day_31/main.py
@@ -2,21 +2,27 @@ from tkinter import *
 from turtle import position 
 import pandas
 import random
+BACKGROUND_COLOR = "#B1DDC6"
 
 data = pandas.read_csv("Data/English_Marathi.csv") 
 to_learn=data.to_dict(orient="records")
-
-
-BACKGROUND_COLOR = "#B1DDC6"
+current_card = {}
 
 def next_card():
+    global current_card
     current_card = random.choice(to_learn)
     canvas.itemconfig(card_title,text="English")
     canvas.itemconfig(card_word,text=current_card["English"])
+def flip_card():
+    canvas.itemconfig(card_title, text="Marathi")
+    canvas.itemconfig(card_word,text=current_card["Marathi"])
+
+
 window = Tk()
 window.title("Flashy")
-
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+window.after(3000,func=flip_card)
 
 canvas = Canvas(width=800,height=529)
 card_front_img = PhotoImage(file="Images/front.png")
