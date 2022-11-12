673daf19eb566a2fea375ef84cac0fd544f976c0
Shree <shreekumarf@gmail.com>
2022-11-12 22:42:07 +0530

    Day-1

--- Python-code/Day_31/main.py
+++ Python-code/Day_31/main.py
@@ -1,3 +1,4 @@
+from textwrap import fill
 from tkinter import *
 from turtle import position 
 import pandas
@@ -9,24 +10,31 @@ to_learn=data.to_dict(orient="records")
 current_card = {}
 
 def next_card():
-    global current_card
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
     current_card = random.choice(to_learn)
     canvas.itemconfig(card_title,text="English")
     canvas.itemconfig(card_word,text=current_card["English"])
+    canvas.itemconfig(card_background,image=card_front_img)
+    flip_timer=window.after(2000,func=flip_card)
+
 def flip_card():
     canvas.itemconfig(card_title, text="Marathi")
     canvas.itemconfig(card_word,text=current_card["Marathi"])
+    canvas.itemconfig(card_background, image=card_back_img)
 
 
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-window.after(3000,func=flip_card)
+flip_timer=window.after(2000,func=flip_card)
+
 
 canvas = Canvas(width=800,height=529)
 card_front_img = PhotoImage(file="Images/front.png")
-canvas.create_image(480,263,image=card_front_img)
+card_back_img= PhotoImage(file="Images/back.png")
+card_background=canvas.create_image(480,263,image=card_front_img)
 card_title = canvas.create_text(400,150,text="",font=("Ariel",40,"italic"))
 card_word = canvas.create_text(400,263,text="",font=("Ariel",60,"italic"))
 canvas.config(bg=BACKGROUND_COLOR,highlightthickness=0)
