58784c06009d937131e038ec8ecef5ac3ec16389
Shree <shreekumarf@gmail.com>
2022-11-13 21:26:26 +0530

    Day-32

--- Python-code/Day_32/main.py
+++ Python-code/Day_32/main.py
@@ -1,5 +1,15 @@
 import smtplib
+from ssl import _PasswordType
 
 my_email = "shree@gmail.com"
-connection = smtplib.SMTP("smtp.gmail.com")
-connection.starttls()
+password = "llihcorv"
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=password)
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs="newuser@gmail.com",
+        msg="Subject:Hello\n\nThis is body of mail..!!"
+    )
+
+
