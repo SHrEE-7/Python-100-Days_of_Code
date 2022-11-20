c82991ed08159942d053f11a8da038c5affbf7dc
Shree <shreekumarf@gmail.com>
2022-11-20 22:13:43 +0530

    Day-32

--- Python-code/Day_32/main.py
+++ Python-code/Day_32/main.py
@@ -6,3 +6,6 @@ today_tuple = (today.month, today.day)
 
 #3 use pandas to read csv
 data = pandas.read_csv("birthday.csv")
+birthday_dict = {(data_row["month"],data_row["day"]):data_row for(index,data_row) in data.iterrows()}
+if today_tuple in birthday_dict:
+    pass
